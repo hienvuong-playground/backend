bc1a50fc41dbda3e09edaec4aa4e1b7a96b91875
Hien Vuong <hienvuong0403@gmail.com>
2026-09-20 15:00:22 +0700

    chore

--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ const app = express();
 app.use(cors());
 
 const SECRET = process.env.SECRET || 'default-secret-value';
+const SECRET2 = process.env.SECRET2 || 'default-secret-value-2';
 
 app.get('/api/hello', (req, res) => {
-  res.json({ message: `Hello from backend! Secret value: ${SECRET}` });
+  res.json({ message: `Hello from backend! Secret value: ${SECRET}, Secret2 value: ${SECRET2}` });
 });
 
 const PORT = process.env.PORT || 3001;
